4eddf749c982c5f2a541631fbe87a4ba18115e96
habiba saad <habibasaad222@icloud.com>
2026-04-02 08:31:15 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
+const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+
 menuOpenButton.addEventListener("click", () => {
-document.body.classList.toggle("show-mobile-menu");
+    document.body.classList.toggle("show-mobile-menu");
 });
 
-menuCloseButton.addEventListener("click", () => menuOpenButton .click());
+menuCloseButton.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove("show-mobile-menu");
+    });
+});
